bcd587653c677c55f765a344e2201a3efbc49f87
zhengzhao <zhaozi486@gmail.com>
2024-12-04 14:25:46 -0800

    sidebar collapsed

--- react-vite/src/components/Sidebar/Sidebar.jsx
+++ react-vite/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { thunkGetAllBoards } from "../../redux/board";
 import OpenModalButton from "../OpenModalButton";
 import ConfirmDeletionModal from "../ConfirmDeletionModal";
+import { FaArrowLeft,FaArrowRight } from "react-icons/fa";
 import "./Sidebar.css";
 
 export default function Sidebar({ onBoardsClick, onBoardSelect }) {
   const dispatch = useDispatch();
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const user = useSelector((state) => state.session.user);
   const boards = useSelector((state) => state.board.allBoards || []);
 
@@ -22,34 +24,51 @@ export default function Sidebar({ onBoardsClick, onBoardSelect }) {
     onBoardsClick();
   };
 
+  const handleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="sidebar">
-      <h2>{user.username}&apos;s Workspace</h2>
-      <h2 className="all-boards" onClick={handleBoardsClick}>
-        Boards
-      </h2>
-      <h2>My Boards</h2>
-      <ul>
-        {boards.length > 0 ? (
-          boards.map((board) => (
-            <li key={board.id} className="sidebar-board">
-              <span onClick={() => handleBoardSelect(board.id)}>
-                {board.title}
-              </span>
-              <div className="board-actions">
-                <OpenModalButton
-                  modalComponent={<ConfirmDeletionModal itemId={board.id} itemType='board' />}
-                  buttonText="Delete"
-                />
-              </div>
-            </li>
-          ))
-        ) : (
-          <p className="no-boards">
-            No boards available. Create a new board to get started!
-          </p>
-        )}
-      </ul>
+    <div className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
+      <div className="sidebar-header">
+        <h2>
+          {user.username}&apos;s Workspace
+        </h2>
+        <div className="collapse-icon" onClick={handleCollapse}>
+          {isCollapsed ? <FaArrowRight /> : <FaArrowLeft />}
+        </div>
+      </div>
+      {!isCollapsed && (
+        <>
+          <h2 className="all-boards" onClick={handleBoardsClick}>
+            Boards
+          </h2>
+          <h2>My Boards</h2>
+          <ul>
+            {boards.length > 0 ? (
+              boards.map((board) => (
+                <li key={board.id} className="sidebar-board">
+                  <span onClick={() => handleBoardSelect(board.id)}>
+                    {board.title}
+                  </span>
+                  <div className="board-actions">
+                    <OpenModalButton
+                      modalComponent={
+                        <ConfirmDeletionModal itemId={board.id} itemType="board" />
+                      }
+                      buttonText="Delete"
+                    />
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p className="no-boards">
+                No boards available. Create a new board to get started!
+              </p>
+            )}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
